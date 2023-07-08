6e26cb09cfd6885b04a9bf220a8e248ca6cd5e64
Aarontendo <69159547+Yeetov@users.noreply.github.com>
2023-07-08 11:49:12 +0100

    Update usrbg.css.js

--- usrbg.css.js
+++ usrbg.css.js
@@ -16,6 +16,10 @@
   background: url(https://cdn.discordapp.com/emojis/929505481989427210.webp) center / 100% 100%;
 }
 
+.avatarStack-3Bjmsl [src^="https://cdn.discordapp.com/avatars/789872551731527690/ec4fedc4e2605ef6cd34cd9d858705d0.webp"] {
+    content: url('https://i.imgur.com/l7hlm5P.gif');
+}
+
 .avatar-31d8He [src^="https://cdn.discordapp.com/avatars/789872551731527690/ec4fedc4e2605ef6cd34cd9d858705d0.webp"] {
     content: url('https://i.imgur.com/l7hlm5P.gif');
 }
